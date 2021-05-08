91658542558baed25037027390498eed1d924b31
Floris Jansen <florismartijnjansen@gmail.com>
2021-05-08 21:42:51 +0200

    allow dragging portrait

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ document.onmousemove = function (e) {mousePos(e);};
 
 var mouseX = 0;
 var mouseY = 0;
+let isMouseDown = false;
 
 function mousePos (e) {
     mouseX = e.pageX - window.pageXOffset;
@@ -47,6 +48,70 @@ function handleOrientation (event) {
 
 
 window.addEventListener('deviceorientation', handleOrientation);
+
+/**
+ * Makes an element draggable., thx @remarkablemark
+ * @param {HTMLElement} element - The element.
+ */
+function draggable (element) {
+
+  if (element === null) return;
+
+  let dragMouseX;
+  let initX = -100;
+  document.documentElement.style.setProperty('--orientation-move', initX);
+
+  // mouse button down over the element
+  element.addEventListener('mousedown', onMouseDown);
+  element.addEventListener('touchstart', onMouseDown);
+  /**
+   * Listens to `mousedown` event.
+   * @param {Object} event - The event.
+   */
+  function onMouseDown(event) {
+    event.preventDefault();
+    isMouseDown = true;
+    prevX = (event.changedTouches) ? event.changedTouches[0].pageX : event.clientX;
+  }
+
+  // mouse button released
+  document.addEventListener('mouseup', onMouseUp);
+  element.addEventListener('touchend', onMouseUp);
+  /**
+   * Listens to `mouseup` event.
+   * @param {Object} event - The event.
+   */
+  function onMouseUp(event) {
+    isMouseDown = false;
+  }
+
+  // need to attach to the entire document
+  // in order to take full width and height
+  // this ensures the element keeps up with the mouse
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('touchmove', onMouseMove);
+
+  /**
+   * Listens to `mousemove` event.
+   * @param {Object} event - The event.
+   */
+  function onMouseMove(event) {
+    if (!isMouseDown) return;
+    let clientX = (event.changedTouches) ? event.changedTouches[0].pageX : event.clientX;
+    let changeX = clientX - prevX;
+    orientationMove = changeX / window.innerWidth * 100 + orientationMove;
+    // if (dragMouseX > 0) dragMouseX = 0;
+    document.documentElement.style.setProperty('--orientation-move', orientationMove);
+    prevX = clientX;
+  }
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  draggable(document.querySelector('.intro__portrait'));
+});
+
+
+
 window.onload = loadIntro();
 function loadIntro() {
   loadPart('1', 1000);
