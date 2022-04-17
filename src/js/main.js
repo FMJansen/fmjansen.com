22f69706976bd11cb28fffc841e8c48feb3fc996
Floris Jansen <florismartijnjansen@gmail.com>
2022-04-17 13:52:45 +0200

    also fire javascript when DOM was cache loaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+if (document.readyState !== 'loading') {
+  init();
+} else {
+  window.addEventListener('DOMContentLoaded', () => {
+    init();
+  });  
+}
+
+function init () {
   document.querySelector('#toggle').onclick = openMenu;
   document.querySelector('#menu').onclick = closeMenu;
   console.log('did I do something stupid? Let me know with a PR or issue 🌸 https://github.com/fmjansen/fmjansen.com');
-});
+}
 
 function openMenu (event) {
   event.preventDefault();
@@ -11,6 +19,7 @@ function openMenu (event) {
 }
 
 function closeMenu (event) {
+  event.preventDefault();
   let menu = document.querySelector('#menu');
   menu.classList.remove('open');
 }
