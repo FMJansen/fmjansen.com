ea3b4d0adc96a9ccb8541fe10ee23a92189cfaf6
Floris Jansen <florismartijnjansen@gmail.com>
2023-02-12 18:00:01 +0100

    jekyll myself

---
+++ github-jekyll.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo "Installing openssl"
+sudo gem install openssl -v '3.1.0' --source 'https://rubygems.org/'
+echo "Gems installed"
+
+echo "Installing gems"
+bundle config path vendor/bundle
+bundle install
+echo "Gems installed"
+
+echo "Jekyll building"
+bundle exec jekyll build
+echo "Jekyll build done"
