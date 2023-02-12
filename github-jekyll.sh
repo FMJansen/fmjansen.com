8221b49831f496d07973bd170a8f905d890bbd8b
Floris Jansen <florismartijnjansen@gmail.com>
2023-02-12 19:37:18 +0100

    install bundler

--- github-jekyll.sh
+++ github-jekyll.sh
@@ -3,6 +3,7 @@ set -e
 
 echo "Installing openssl"
 sudo gem install openssl -v '3.1.0' --source 'https://rubygems.org/'
+sudo gem install bundler jekyll
 echo "Gems installed"
 
 echo "Installing gems"
